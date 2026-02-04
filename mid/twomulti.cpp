3103027b6e8310473ef51e91bc9bcde963d375f6
Tanzimul islam <tanzimul3010@gmail.com>
2026-02-04 12:51:55 +0600

    Add multiplication code

---
+++ mid/twomulti.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int x,y;
+    cout << "Enter first number: ";
+    cin >> x;
+    cout << "Enter second number: ";
+    cin >> y;
+    cout << "Multiplication of two numbers: " << x*y << endl;
+}
